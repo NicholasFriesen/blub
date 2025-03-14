3aca6ca13992df233afcd700900455a7a79cf661
Nicholas Josiah Friesen <36311855+NicholasFriesen@users.noreply.github.com>
2025-03-13 20:40:07 -0600

    Update blub.py

--- blub.py
+++ blub.py
@@ -1,35 +1,30 @@
-#imports
+# imports
 import random
 import time
 
-#fish game
+# fish game
 color = ('red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet')
 pattern = ('striped', 'spotted', 'splotched', 'wavy', 'leopard', 'solid')
 texture = ('slimey', 'scaley', 'smooth', 'shiny', 'spiked', 'rough')
 
-colorArray = ('red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet')
-patternArray = ('striped', 'spotted', 'splotched', 'wavy', 'leopard', 'solid')
-textureArray = ('slimey', 'scaley', 'smooth', 'shiny', 'spiked', 'rough')
-
+colorArray = color
+patternArray = pattern
+textureArray = texture
 
 class Shop:
-#shop attributes and traits  
-                 
     def __init__(self, name, slots):
         self.name = name
         self.slots = slots
 
-#Shops
+# Shops
 fishMart = Shop('Fish Mart', 3)
 luresRUs = Shop('Lures R Us', 6)
 captainCo = Shop('Captain Co.', 9)
 
-#Shop Array
+# Shop Array
 shopArray = (fishMart, luresRUs, captainCo)
 
 class Hook:
-#hook attributes and traits  
-                 
     def __init__(self, name, valueMin, valueMax):
         self.name = name
         self.valueMin = valueMin
@@ -38,9 +33,9 @@ class Hook:
         self.color = random.choice(color)
         self.pattern = random.choice(pattern)
         self.texture = random.choice(texture)
-        self.ID = str(self.texture) + ' ' + str(self.color) + ' ' + str(self.pattern) + ' ' + str(self.name)
+        self.ID = f'{self.texture} {self.color} {self.pattern} {self.name}'
 
-#Hooks
+# Hooks
 spinner = Hook('spinner', 10, 20)
 jig = Hook('jig', 20, 30)
 treble = Hook('treble hook', 50, 100)
@@ -51,19 +46,16 @@ brass = Hook('brass lure', 90, 100)
 spear = Hook('spear', 100, 1000)
 harpoon = Hook('harpoon', 1000, 99999)
 
-#Hook Array
 hookArray = (spinner, jig, treble, single, spoon, wooden, brass, spear, harpoon)
 
 class Fish:
-#fish attributes and traits  
-                 
     def __init__(self, name, valueMin, valueMax):
         self.name = name
         self.valueMin = valueMin
         self.valueMax = valueMax
         self.value = random.randint(valueMin, valueMax)
 
-#Fish
+# Fish
 trout = Fish('Trout', 1, 50)
 salmon = Fish('Salmon', 5, 130)
 walley = Fish('Walley', 1, 20)
@@ -80,23 +72,19 @@ pike = Fish('Pike', 1, 70)
 muskie = Fish('Muskie', 5, 70)
 
 class Other:
-#other attributes and traits  
-                 
     def __init__(self, name, valueMin, valueMax):
         self.name = name
         self.valueMin = valueMin
         self.valueMax = valueMax
         self.value = random.randint(self.valueMin, self.valueMax)
-#Other
+
 boot = Other('Boot', 0, 0)
 weeds = Other('Weeds', 0, 0)
 jewel = Other('Jewel', 25, 50)
-line = Other('Old Line', 0, 0) 
+line = Other('Old Line', 0, 0)
 
-#Other Array
 otherArray = (boot, weeds, jewel, line)
 
-#Location Arrays
 lakeArray = (trout, pike, walley, minnow, burbot, muskie, boot, weeds, jewel, treble, single, spoon, spinner, jig)
 oceanArray = (salmon, marlin, lionfish, shark, whale, harpoon, spear)
 riverArray = (trout, whitefish, salmon, minnow, pike, sturgeon, muskie, weeds, treble, spoon, jig, brass, wooden)
@@ -104,244 +92,176 @@ rapidsArray = (trout, salmon, sturgeon, single, treble)
 pondArray = (minnow, carp, boot, weeds, jewel, line, spinner, spoon, jig)
 
 class Location:
-#location attributes and traits  
-                 
     def __init__(self, name, fee, items):
         self.name = name
         self.fee = fee
         self.items = items
 
-#Locations
 lake = Location('lake', 10, lakeArray)
 ocean = Location('ocean', 1500, oceanArray)
 river = Location('river', 150, riverArray)
 rapids = Location('rapids', 250, rapidsArray)
 pond = Location('pond', 5, pondArray)
 
-#Location Array
-locationArray = [lake, ocean, river, rapids, pond]    
+locationArray = [lake, ocean, river, rapids, pond]
 
 def Slots(shop, hook, gold):
-    time.sleep(.5)
-    print ' '
-    print ('Current Shop: ' + shop.name) 
-    print ' '
+    time.sleep(0.5)
+    print('\n')
+    print(f'Current Shop: {shop.name}\n')
     shopList = []
     counter = 0
     while counter < shop.slots:
-        counter = counter + 1
+        counter += 1
         slotItem = random.choice(hookArray)
         shopList.append(slotItem)
-    for item in shopList: 
-        time.sleep(.5)
-        #item.color = random.choice(color)
-        #item.pattern = random.choice(pattern)
-        #item.texture = random.choice(texture)
-        #item.value = random.randint(item.valueMin, item.valueMax)
-        #item.ID = str(item.texture) + ' ' + str(item.color) + ' ' + str(item.pattern) + ' ' + str(item.name)
-        print str(item.ID) + '($' + str(item.value) + ')'
-    print ' '
+    for item in shopList:
+        time.sleep(0.5)
+        print(f'{item.ID} (${item.value})')
+    print('\n')
     time.sleep(1)
-    print ('Current Gold: ($' + str(gold) + ')')
-    print ('Your current hook:')
-    print (hook.ID + '(' + str(hook.value) + ' dmg)')       
-    print ('Choose item or leave?')
-    print ' '
-    loop = 'none' 
-    while loop == 'none':        
-        choice = raw_input()
+    print(f'Current Gold: (${gold})')
+    print('Your current hook:')
+    print(f'{hook.ID} ({hook.value} dmg)')
+    print('Choose item or leave?\n')
+
+    loop = 'none'
+    while loop == 'none':
+        choice = input()
         for item in shopList:
-            if choice == item.ID: 
+            if choice == item.ID:
                 if gold > item.value:
                     loop = 'done'
                     oldHook = hook
-                    hook = item         
-                    gold = gold - hook.value 
-                    time.sleep(.5)
-                    print ('You got rid of the:')
-                    print oldHook.ID
-                    print ' '
-                    time.sleep(.5)
-                    print ('You now have the:')
-                    print hook.ID
-                    print ' '
-                    time.sleep(.5)
-                    print ('You now have ' + str(gold) + ' gold.')
-                    print ' '
-                    time.sleep(.5)
+                    hook = item
+                    gold -= hook.value
+                    time.sleep(0.5)
+                    print(f'You got rid of the: {oldHook.ID}\n')
+                    time.sleep(0.5)
+                    print(f'You now have the: {hook.ID}\n')
+                    time.sleep(0.5)
+                    print(f'You now have {gold} gold.\n')
+                    time.sleep(0.5)
                     Choose(gold, hook)
                 else:
                     loop = 'done'
-                    print ('You do not have sufficient funds.')
-                    print ' '
-                    time.sleep(.5)
+                    print('You do not have sufficient funds.\n')
+                    time.sleep(0.5)
                     Choose(gold, hook)
             elif choice == 'leave':
-                loop = 'none'
+                loop = 'done'
                 Choose(gold, hook)
-            else:
-                loop = 'none'
-                print ' '
-            
+
 def Spot(gold, hook):
-    print ' ' 
-    print ('   Fishing Destinations:')  
-    print ' '
-    for location in locationArray: 
-        time.sleep(.5)
-        print str(location.name) + '($' + str(location.fee) + ')'
+    print('\n   Fishing Destinations:\n')
+    for location in locationArray:
+        time.sleep(0.5)
+        print(f'{location.name} (${location.fee})')
     time.sleep(1)
-    print ' '
-    print ('Current Gold: ($' + str(gold) + ')')       
-    print ('Which do you choose?')    
-    print ' '
-    loop = 'none' 
-    while loop == 'none':        
-        choice = raw_input()
+    print('\n')
+    print(f'Current Gold: (${gold})')
+    print('Which do you choose?\n')
+
+    loop = 'none'
+    while loop == 'none':
+        choice = input()
         for location in locationArray:
-            if choice == location.name: 
+            if choice == location.name:
                 if gold > location.fee:
-                    loop = 'done'                    
+                    loop = 'done'
                     destination = location
-                    gold = gold - destination.fee
-                    time.sleep(.5)
-                    print ('You now have ' + str(gold) + ' gold.')
-                    print ' '
+                    gold -= destination.fee
+                    time.sleep(0.5)
+                    print(f'You now have {gold} gold.\n')
                     Fishing(destination, gold, hook)
                 else:
                     loop = 'done'
-                    print ('You do not have sufficient funds.')
-                    print ' '
-                    time.sleep(.5)
+                    print('You do not have sufficient funds.\n')
+                    time.sleep(0.5)
                     Choose(gold, hook)
-            else:
-                loop = 'none'         
-    
+
 def Fishing(location, gold, hook):
-    time.sleep(.5)
-    print ' '
-    print ('Current Fishing Spot: ' + location.name) 
-    print ' '
-    while gold > 0 or fish == 'fish':
-       outcome = random.choice(location.items)  
-       #outcome.value = random.randint(outcome.valueMin, outcome.valueMax)   
-       if outcome in hookArray:
-           #outcome.color = random.choice(color)
-           #outcome.pattern = random.choice(pattern)
-           #outcome.texture = random.choice(texture)
-           #outcome.ID = str(outcome.texture) + ' ' + str(outcome.color) + ' ' + str(outcome.pattern) + ' ' + str(outcome.name)
-           time.sleep(.5)
-           print ('You found a:')
-           print (outcome.ID + '(' + str(outcome.value) + ' dmg)')
-           time.sleep(.5)
-           print ' '
-           print ('Your current hook:')
-           print (hook.ID + '(' + str(hook.value) + ' dmg)')       
-           time.sleep(1)
-           print ('   switch or keep?')
-           choice = raw_input()
-           if choice == 'switch':
-               oldHook = hook
-               hook = outcome
-               location = location   
-       elif outcome in otherArray:
-           outcome.value = random.randint(outcome.valueMin, outcome.valueMax)   
-           print ' '
-           time.sleep(.5)
-           print ('you found: ' + outcome.name + '(' + str(outcome.value) + ' lbs.)')
-           gold = gold + outcome.value
-           print ('your gold: ($' + str(gold) + ')')
-           time.sleep(1)
-           print ('   fish or leave?')
-           fish = raw_input()
-           outcome = random.choice(location.items)     
-           if fish == 'leave':
-               Choose(gold, hook)
-                      
-       elif hook.value > outcome.value:
-           outcome.value = random.randint(outcome.valueMin, outcome.valueMax)   
-           print ' '
-           time.sleep(.5)
-           print ('you found: ' + outcome.name + '(' + str(outcome.value) + ' lbs.)')
-           gold = gold + outcome.value           
-           print ('your gold: ($' + str(gold) + ')')
-           time.sleep(1)           
-           print ('   fish or leave?')
-           fish = raw_input()
-           if fish == 'leave':
-               Choose(gold, hook)
-       else: 
-           print ' '
-           time.sleep(.5)
-           print ('your ' + hook.ID + ' broke from a ' + outcome.name + '(' + str(outcome.value) + 'lbs.)') 
-           break  
-    
+    time.sleep(0.5)
+    print('\n')
+    print(f'Current Fishing Spot: {location.name}\n')
+    fish = 'fish'
+    while gold > 0 and fish == 'fish':
+        outcome = random.choice(location.items)
+        if outcome in hookArray:
+            time.sleep(0.5)
+            print(f'You found a:\n{outcome.ID} ({outcome.value} dmg)\n')
+            time.sleep(0.5)
+            print(f'Your current hook:\n{hook.ID} ({hook.value} dmg)\n')
+            time.sleep(1)
+            print('   switch or keep?')
+            choice = input()
+            if choice == 'switch':
+                oldHook = hook
+                hook = outcome
+        elif outcome in otherArray:
+            outcome.value = random.randint(outcome.valueMin, outcome.valueMax)
+            print(f'\nYou found: {outcome.name} ({outcome.value} lbs.)')
+            gold += outcome.value
+            print(f'Your gold: (${gold})')
+            time.sleep(1)
+            print('   fish or leave?')
+            fish = input()
+            if fish == 'leave':
+                Choose(gold, hook)
+        elif hook.value > outcome.value:
+            outcome.value = random.randint(outcome.valueMin, outcome.valueMax)
+            print(f'\nYou found: {outcome.name} ({outcome.value} lbs.)')
+            gold += outcome.value
+            print(f'Your gold: (${gold})')
+            time.sleep(1)
+            print('   fish or leave?')
+            fish = input()
+            if fish == 'leave':
+                Choose(gold, hook)
+        else:
+            print(f'\nYour {hook.ID} broke from a {outcome.name} ({outcome.value} lbs.)')
+            break
+
 def Intro():
     global gold
     gold = 1000
     global hook
     startArray = (jig, spinner)
-    hook = random.choice(startArray)        
-    time.sleep(.5)
-    print ' '
-    print ('---Fishing Adventure!---')
-    time.sleep(.5)
-    print ' '
-    print ('   RULES:')
-    time.sleep(.5)
-    print ('   Earn Money.')
-    print ('   Find Hooks.')
-    print ('   Catch Fish.')
+    hook = random.choice(startArray)
+    time.sleep(0.5)
+    print('\n---Fishing Adventure!---\n')
+    time.sleep(0.5)
+    print('   RULES:')
+    time.sleep(0.5)
+    print('   Earn Money.')
+    print('   Find Hooks.')
+    print('   Catch Fish.')
     time.sleep(1)
-    print ' '
+    print('\n')
+    print('Your current hook:')
+    print(f'{hook.ID} ({hook.value} dmg)')
+    time.sleep(1)
+    print('\n')
 
-    print ('Your current hook:')
-    print (hook.ID + '(' + str(hook.value) + ' dmg)')       
-    time.sleep(1)
-    print ' '
-    
-def Choose(gold, hook): 
- 
-    print ('   Choose: fishing or shop')
+def Choose(gold, hook):
+    print('   Choose: fishing or shop')
     loop = 'none'
     while loop == 'none':
-        go = raw_input()
-    
+        go = input()
         if go == 'shop':
             loop = 'done'
             shop = random.choice(shopArray)
-            Slots(shop, hook, gold)  
+            Slots(shop, hook, gold)
         elif go == 'fishing':
             loop = 'done'
             Spot(gold, hook)
-        else:  
+        else:
             loop = 'none'
-            print ' '
-            print ('Please enter a valid response.')      
-            print ' '
+            print('\nPlease enter a valid response.\n')
 
 def Game():
     Intro()
     Choose(gold, hook)
 
-print spinner
-print spinner.color
-spinner.color = random.choice(color)
-print spinner
-print spinner.color
-spinner.color = random.choice(colorArray)
-print spinner
-print spinner.color
-print jig
-print jig.color
-test = jig
-print test
-print test.color
-jig.color = random.choice(colorArray)
-print jig
-print jig.color
-test.color = random.choice(colorArray)
-print test
-print test.color
-
+# Run the game
 Game()
